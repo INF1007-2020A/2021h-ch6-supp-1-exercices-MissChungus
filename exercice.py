edbba7e63043c364ac483acc5d55dd548a604850
MissChungus <telesanti58@hotmail.com>
2021-02-20 18:58:21 -0500

    exos 1, 2,4 complétés

--- exercice.py
+++ exercice.py
@@ -8,16 +8,32 @@ import itertools
 
 
 def get_maximums(numbers):
-	return [0]
+
+	return [max(element) for element in numbers]
 
 def join_integers(numbers):
-	return 0
+	entier = ''
+	for number in numbers:
+		entier += str(number)
+	entier = int(entier)
+
+	return entier
 
 def generate_prime_numbers(limit):
 	return [0]
 
 def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
-	return [""]
+	result = []
+	for num in range(1, num_combinations + 1):
+		if excluded_multiples == None:
+			for letter in strings:
+				result.append(letter + str(num))
+		else:
+			if num % excluded_multiples != 0:
+				for letter in strings:
+					result.append(letter + str(num))
+
+	return result
 
 if __name__ == "__main__":
 	print(get_maximums([[1,2,3], [6,5,4], [10,11,12], [8,9,7]]))
